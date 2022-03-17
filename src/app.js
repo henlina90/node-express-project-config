0ecf1fafed15ce72e886fbe1e2493f645fc84ea4
henlina90 <henlina90>
2022-03-16 17:19:32 -0700

    installed morgan pckg and cleaned up comments

--- src/app.js
+++ src/app.js
@@ -1,10 +1,16 @@
 // The app.js file is where you will configure your Express application.
-
-// You require the Express package and assign it to a variable.
 const express = require("express");
 
-// The Express package exports a function. When you invoke that function, you get a new Express application and assign it to a variable.
 const app = express();
 
-// You export the Express application to be used in the server.js file.
+const morgan = require("morgan");
+
+const sayHello = (req, res, next) => {
+  res.send("Hello!");
+};
+
+// Attach middleware to the pipeline
+app.use(morgan("dev"));
+app.use(sayHello);
+
 module.exports = app;
